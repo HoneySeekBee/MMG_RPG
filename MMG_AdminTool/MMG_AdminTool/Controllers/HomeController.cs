d4712cc2e041dbb465f8fc04174abddab57ff213
HoneySeekBee <dosik112290@gmail.com>
2025-08-05 14:38:26 +0900

    [MMG_RPG] 운영툴 디자인 변경

--- MMG_AdminTool/MMG_AdminTool/Controllers/HomeController.cs
+++ MMG_AdminTool/MMG_AdminTool/Controllers/HomeController.cs
@@ -12,8 +12,6 @@ namespace MMG_AdminTool.Controllers
         public IActionResult Test()
         {
             ViewData["MyMsg"] = "Hello response";
-            //ViewBag.MyTest = new List<string> { "abc", "def", "ton" };
-            //ViewBag.MyNum = 5;
 
             var list = new List<string> { "abc", "def", "ton" };
 
